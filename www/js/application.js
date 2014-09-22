79b0ba8f8f57997f30aa284b7d74ba0046f65dc1
lcfc <informatica@lcfc.it>
2014-09-22 19:13:27 +0200

    Update application.js

--- www/js/application.js
+++ www/js/application.js
@@ -14,7 +14,7 @@ var Application = {
   },
 
   onDeviceReady: function() {
-  alert(window.navigator.userAgent);
+  alert(JSON.stringify(window.device));
     Application.orientationChange();
     var contentScroll = new iScroll('scroll');
     window.gaInterval = setInterval(function(){
