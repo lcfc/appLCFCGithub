b1d57102a0d6a048b88a497b1507a104a8e9e533
lcfc <paolocantoni@MacBook-Air.local>
2014-10-25 19:45:48 +0200

    aaa

--- www/js/application.js
+++ www/js/application.js
@@ -1988,7 +1988,7 @@ var Application = {
 
     //invio foto selezionate
     $("#foto").on("click", "#foto-invia", function() {
-      $("#foto-invio-esito").html("Invio immagini in corso...").css({'padding': '5px', 'border':'1px solid #fc0', 'background': '#ffc'});
+      $("#foto-invio-esito").html("Invio immagini in corso...").css({'margin-bottom': '5px', 'padding': '5px', 'border':'1px solid #fc0', 'background': '#ffc'});
       var params = {};
       params.titolo = $("#foto-titolo").val();
       params.descrizione = $("#foto-descrizione").val();
@@ -2007,7 +2007,7 @@ var Application = {
         ft.upload(fileUrl, encodeURI(urlGestionale+"stampa/uploadFotoFromApp"), Application.onUploadFile, Application.onFailUploadFile, options);
         $(this).parent().remove();
       });
-      $("#foto-invio-esito").html("Invio immagini completato");
+      $("#foto-invio-esito").html("Invio immagini completato").fadeOut(4000);
     });
 
     $("#foto-anteprime").on("click", "div", function(){
